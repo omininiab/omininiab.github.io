f775a4ba266c29c34701406fabf79bf3da077a04
Ominini <omininiab@gmail.com>
2021-01-16 01:15:43 -0500

    client side delivery of projects

---
+++ js/projects.js
@@ -0,0 +1,39 @@
+let projects = [
+  {
+    title: "Etch-a-Sketch",
+    url: "https://www.ominini.com/etch-a-sketch",
+    img: "/img/etch-a-sketch.png",
+  },
+  {
+    title: "Rock Paper Scissors",
+    url: "https://www.ominini.com/rock-paper-scissors",
+    img: "/img/rock-paper-scissors.png",
+  },
+  {
+    title: "Google Homepage",
+    url: "https://www.ominini.com/google-homepage",
+    img: "/img/google-homepage.png",
+  },
+  {
+    title: "Survey Form",
+    url: "https://www.ominini.com/survey-form",
+    img: "/img/survey-form.png",
+  },
+  {
+    title: "Ravi Tribute Page",
+    url: "https://www.ominini.com/ravi-tribute-page",
+    img: "/img/ravi-tribute-page.png",
+  },
+];
+
+const projectList = document.querySelector("#projectList");
+
+projects.forEach(function(project) {
+  let aProject = document.createElement("a");
+  aProject.classList.add("project-tile");
+  aProject.classList.add("flex-item");
+  aProject.target = "_blank";
+  aProject.href = project.url;
+  aProject.innerHTML = `<img class="project-image" src=${project.img} /><p class="project-title">${project.title}</p>`;
+  projectList.appendChild(aProject);
+})
