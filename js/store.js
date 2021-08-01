991ab24b8401a31af5171c0ca7502406321957f6
omininiab <info@ominini.com>
2021-08-01 01:45:47 -0400

    fix homepage with streamlined product line

--- js/store.js
+++ js/store.js
@@ -1,7 +1,6 @@
 /*<![CDATA[*/
 (function () {
-  var scriptURL =
-    "https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js";
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
   if (window.ShopifyBuy) {
     if (window.ShopifyBuy.UI) {
       ShopifyBuyInit();
@@ -12,195 +11,182 @@
     loadScript();
   }
   function loadScript() {
-    var script = document.createElement("script");
+    var script = document.createElement('script');
     script.async = true;
     script.src = scriptURL;
-    (
-      document.getElementsByTagName("head")[0] ||
-      document.getElementsByTagName("body")[0]
-    ).appendChild(script);
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
     script.onload = ShopifyBuyInit;
   }
   function ShopifyBuyInit() {
     var client = ShopifyBuy.buildClient({
-      domain: "ominini.myshopify.com",
-      storefrontAccessToken: "2792609daf9b1251ad64815bb7e58406",
+      domain: 'ominini.myshopify.com',
+      storefrontAccessToken: '2792609daf9b1251ad64815bb7e58406',
     });
     ShopifyBuy.UI.onReady(client).then(function (ui) {
-      ui.createComponent("collection", {
-        id: "270260437186",
-        node: document.getElementById("collection-component-1624853562575"),
-        moneyFormat: "%24%7B%7Bamount%7D%7D",
+      ui.createComponent('collection', {
+        id: '274702205122',
+        node: document.getElementById('collection-component-1627796607952'),
+        moneyFormat: '%24%7B%7Bamount%7D%7D',
         options: {
-          product: {
-            styles: {
-              product: {
+          "product": {
+            "styles": {
+              "product": {
                 "@media (min-width: 601px)": {
-                  "max-width": "calc(50% - 40px)",
-                  "margin-left": "40px",
+                  "max-width": "calc(25% - 20px)",
+                  "margin-left": "20px",
                   "margin-bottom": "50px",
-                  width: "calc(50% - 40px)",
-                },
-                img: {
-                  height: "calc(100% - 15px)",
-                  position: "absolute",
-                  left: "0",
-                  right: "0",
-                  top: "0",
-                },
-                imgWrapper: {
-                  "padding-top": "calc(75% + 15px)",
-                  position: "relative",
-                  height: "0",
-                },
+                  "width": "calc(25% - 20px)"
+                }
               },
-              title: {
-                "font-family": "Poppins, sans-serif",
+              "title": {
+                "font-family": "Roboto, sans-serif"
               },
-              button: {
-                "font-family": "Poppins, sans-serif",
-                "font-weight": "normal",
+              "button": {
+                "font-family": "Lato, sans-serif",
                 ":hover": {
-                  "background-color": "#000000",
+                  "background-color": "#000000"
                 },
                 "background-color": "#000000",
                 ":focus": {
-                  "background-color": "#000000",
-                },
+                  "background-color": "#000000"
+                }
               },
-              price: {
-                "font-family": "Poppins, sans-serif",
+              "price": {
+                "font-family": "Roboto, sans-serif"
               },
-              compareAt: {
-                "font-family": "Poppins, sans-serif",
+              "compareAt": {
+                "font-family": "Roboto, sans-serif"
               },
-              unitPrice: {
-                "font-family": "Poppins, sans-serif",
-              },
+              "unitPrice": {
+                "font-family": "Roboto, sans-serif"
+              }
             },
-            contents: {
-              button: false,
-              buttonWithQuantity: true,
+            "buttonDestination": "modal",
+            "contents": {
+              "options": false
             },
-            text: {
-              button: "Add to Cart",
+            "width": "580px",
+            "text": {
+              "button": "View product"
             },
-            googleFonts: ["Poppins"],
+            "googleFonts": [
+              "Roboto",
+              "Lato"
+            ]
           },
-          productSet: {
-            styles: {
-              products: {
+          "productSet": {
+            "styles": {
+              "products": {
                 "@media (min-width: 601px)": {
-                  "margin-left": "-40px",
-                },
-              },
-            },
+                  "margin-left": "-20px"
+                }
+              }
+            }
           },
-          modalProduct: {
-            contents: {
-              img: false,
-              imgWithCarousel: true,
-              button: false,
-              buttonWithQuantity: true,
+          "modalProduct": {
+            "contents": {
+              "img": false,
+              "imgWithCarousel": true,
+              "button": false,
+              "buttonWithQuantity": true
             },
-            styles: {
-              product: {
+            "styles": {
+              "product": {
                 "@media (min-width: 601px)": {
                   "max-width": "100%",
                   "margin-left": "0px",
-                  "margin-bottom": "0px",
-                },
+                  "margin-bottom": "0px"
+                }
               },
-              button: {
-                "font-family": "Poppins, sans-serif",
-                "font-weight": "normal",
+              "button": {
+                "font-family": "Lato, sans-serif",
                 ":hover": {
-                  "background-color": "#000000",
+                  "background-color": "#000000"
                 },
                 "background-color": "#000000",
                 ":focus": {
-                  "background-color": "#000000",
-                },
+                  "background-color": "#000000"
+                }
               },
-              title: {
-                "font-family": "Poppins, sans-serif",
-                "font-weight": "normal",
+              "title": {
+                "font-family": "Helvetica Neue, sans-serif",
+                "font-weight": "bold",
                 "font-size": "26px",
-                color: "#4c4c4c",
+                "color": "#4c4c4c"
               },
-              price: {
-                "font-family": "Poppins, sans-serif",
+              "price": {
+                "font-family": "Helvetica Neue, sans-serif",
                 "font-weight": "normal",
                 "font-size": "18px",
-                color: "#4c4c4c",
+                "color": "#4c4c4c"
               },
-              compareAt: {
-                "font-family": "Poppins, sans-serif",
+              "compareAt": {
+                "font-family": "Helvetica Neue, sans-serif",
                 "font-weight": "normal",
                 "font-size": "15.299999999999999px",
-                color: "#4c4c4c",
+                "color": "#4c4c4c"
               },
-              unitPrice: {
-                "font-family": "Poppins, sans-serif",
+              "unitPrice": {
+                "font-family": "Helvetica Neue, sans-serif",
                 "font-weight": "normal",
                 "font-size": "15.299999999999999px",
-                color: "#4c4c4c",
-              },
+                "color": "#4c4c4c"
+              }
             },
-            googleFonts: ["Poppins"],
-            text: {
-              button: "Add to cart",
-            },
+            "googleFonts": [
+              "Lato"
+            ],
+            "text": {
+              "button": "Add to cart"
+            }
           },
-          option: {
-            styles: {
-              label: {
-                "font-family": "Poppins, sans-serif",
-              },
-              select: {
-                "font-family": "Poppins, sans-serif",
-              },
-            },
-            googleFonts: ["Poppins"],
+          "option": {
+            "googleFonts": [
+              "Roboto"
+            ]
           },
-          cart: {
-            styles: {
-              button: {
-                "font-family": "Poppins, sans-serif",
+          "cart": {
+            "styles": {
+              "button": {
+                "font-family": "Lato, sans-serif",
                 ":hover": {
-                  "background-color": "#000000",
+                  "background-color": "#000000"
                 },
                 "background-color": "#000000",
                 ":focus": {
-                  "background-color": "#000000",
-                },
-              },
+                  "background-color": "#000000"
+                }
+              }
             },
-            text: {
-              total: "Subtotal",
-              button: "Checkout",
+            "text": {
+              "total": "Subtotal",
+              "button": "Checkout"
             },
-            popup: false,
-            googleFonts: ["Poppins"],
+            "popup": false,
+            "googleFonts": [
+              "Lato"
+            ]
           },
-          toggle: {
-            styles: {
-              toggle: {
-                "font-family": "Poppins, sans-serif",
+          "toggle": {
+            "styles": {
+              "toggle": {
+                "font-family": "Lato, sans-serif",
                 "background-color": "#000000",
                 ":hover": {
-                  "background-color": "#000000",
+                  "background-color": "#000000"
                 },
                 ":focus": {
-                  "background-color": "#000000",
-                },
-              },
+                  "background-color": "#000000"
+                }
+              }
             },
-            googleFonts: ["Poppins"],
-          },
+            "googleFonts": [
+              "Lato"
+            ]
+          }
         },
       });
     });
   }
 })();
-/*]]>*/
+  /*]]>*/
