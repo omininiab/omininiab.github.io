0e36d7631a43d5ea9f7554a9c05f38a05502fda6
Ominini <ominini@duck.com>
2022-04-16 14:24:31 -0400

    new layout with preloader for homepage

--- js/store.js
+++ js/store.js
@@ -10,6 +10,7 @@
   } else {
     loadScript();
   }
+
   function loadScript() {
     var script = document.createElement('script');
     script.async = true;
@@ -17,6 +18,7 @@
     (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
     script.onload = ShopifyBuyInit;
   }
+  
   function ShopifyBuyInit() {
     var client = ShopifyBuy.buildClient({
       domain: 'ominini.myshopify.com',
